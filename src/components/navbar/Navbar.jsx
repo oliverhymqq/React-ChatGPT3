6e467e69efab0d68a668f67f5e211a940f193508
oliverhymqq <101374173+oliverhymqq@users.noreply.github.com>
2023-06-20 12:41:07 +1000

    chatGPT3

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import './navbar.css'
+import { RiMenu3Line, RiCloseCircleLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#features">Open Ai</a>
+          </p>
+          <p>
+            <a href="#possibility">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseCircleLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <p>
+                <a href="#home">Home</a>
+              </p>
+              <p>
+                <a href="#wgpt3">What is GPT3?</a>
+              </p>
+              <p>
+                <a href="#possibility">Open AI</a>
+              </p>
+              <p>
+                <a href="#features">Case Studies</a>
+              </p>
+              <p>
+                <a href="#blog">Library</a>
+              </p>
+            </div>
+            <div className="gpt3__navbar-menu_container-links-sign">
+              <p>sign in</p>
+              <button type="button">Sign Up</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
